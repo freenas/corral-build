9016e00ada6a5714efcdc96b1a2edf720ded759e
John Hixson <john@ixsystems.com>
2016-09-13 20:07:55 -0700

    Take UIDs and GIDs into account when creating ports overlay
    (cherry picked from commit 980f88f0b04f82ce46e7e07fa18033f89426ebf2)

--- build/tools/build-ports.py
+++ build/tools/build-ports.py
@@ -141,10 +141,17 @@ def merge_port_trees():
         info(e('Merging ports tree ${i}'))
         for p in glob('${i}/*/*'):
             portpath = '/'.join(p.split('/')[-2:])
+            uids = "%s/%s" % (i, "UIDs")
+            gids = "%s/%s" % (i, "GIDs")
+
             sh('rm -rf ${PORTS_OVERLAY}/${portpath}')
             sh('mkdir -p ${PORTS_OVERLAY}/${portpath}')
             sh('cp -lr ${p}/ ${PORTS_OVERLAY}/${portpath}')
 
+            if os.path.exists(uids):
+                sh('cp ${uids} ${PORTS_OVERLAY}/UIDs')
+            if os.path.exists(gids):
+                sh('cp ${gids} ${PORTS_OVERLAY}/GIDs')
 
 def prepare_env():
     for cmd in jailconf.get('copy', []):
